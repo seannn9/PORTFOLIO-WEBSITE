740b4eb321fb6be7d245766238a159e801f4e86f
seannn9 <naeskirlu9@gmail.com>
2025-07-16 22:26:10 +0800

    fetching projects from supabase table

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,10 +1,44 @@
-import ScrollDown from "../../components/ScrollDown/ScrollDown.jsx";
 import "./Projects.css";
+import supabase from "../../server/supabase.js";
+import { useState, useEffect } from "react";
+import ProjectCard from "../../components/ProjectCard/ProjectCard.jsx";
+import ScrollDown from "../../components/ScrollDown/ScrollDown.jsx";
 
 export default function Projects() {
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        fetchProjects();
+    }, []);
+
+    const fetchProjects = async () => {
+        try {
+            const { data, error } = await supabase.from("Projects").select("*");
+            if (error) {
+                console.log(
+                    "An error occured while fetching projects: ",
+                    error
+                );
+            } else {
+                setProjects(data);
+            }
+        } catch (error) {
+            console.log("An error occured while fetching projects: ", error);
+        } finally {
+        }
+    };
+
     return (
         <section id="projects-section" className="projects-section">
             <h2 className="projects-header">PROJECTS</h2>
+            <div className="project-container">
+                {projects.map((project, key) => (
+                    <ProjectCard
+                        key={key}
+                        project_name={project.project_name}
+                    />
+                ))}
+            </div>
         </section>
     );
 }
