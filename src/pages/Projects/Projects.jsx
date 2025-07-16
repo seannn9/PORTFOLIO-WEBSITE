4cca1005deaf898f27d0e154792c3663314e6c1d
seannn9 <naeskirlu9@gmail.com>
2025-07-16 19:19:34 +0800

    replaced skills with projects section

---
+++ src/pages/Projects/Projects.jsx
@@ -0,0 +1,14 @@
+import ScrollDown from "../../components/ScrollDown/ScrollDown.jsx";
+import "./Projects.css";
+
+export default function Projects() {
+    return (
+        <section id="projects-section" className="projects-section">
+            <h2 className="projects-header">PROJECTS</h2>
+        </section>
+    );
+}
+
+{
+    /* <ScrollDown section_id="projects-section" color_stroke="#1b1b1b" /> */
+}
