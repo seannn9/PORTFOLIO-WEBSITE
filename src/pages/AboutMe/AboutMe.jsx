783c4d446b1de96735ebb75a9188016535639b47
seannn9 <seanulric9@gmail.com>
2025-07-23 15:52:55 +0800

    style: minor changes on color of svgs

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -23,7 +23,7 @@ export default function AboutMe() {
                         >
                             <path
                                 d="M16 18V17C16 15.8954 16.8954 15 18 15C19.1046 15 20 15.8954 20 17V18M16.9527 11C16.9839 10.7366 17 10.47 17 10.2C17 6.22355 13.866 3 10 3C6.13401 3 3 6.22355 3 10.2C3 14.1764 6.5 17.4 10 21C10.3344 20.6561 10.6687 20.3156 11 19.9776M16.5 21H19.5C19.9659 21 20.1989 21 20.3827 20.9239C20.6277 20.8224 20.8224 20.6277 20.9239 20.3827C21 20.1989 21 19.9659 21 19.5C21 19.0341 21 18.8011 20.9239 18.6173C20.8224 18.3723 20.6277 18.1776 20.3827 18.0761C20.1989 18 19.9659 18 19.5 18H16.5C16.0341 18 15.8011 18 15.6173 18.0761C15.3723 18.1776 15.1776 18.3723 15.0761 18.6173C15 18.8011 15 19.0341 15 19.5C15 19.9659 15 20.1989 15.0761 20.3827C15.1776 20.6277 15.3723 20.8224 15.6173 20.9239C15.8011 21 16.0341 21 16.5 21ZM13 10C13 11.6569 11.6569 13 10 13C8.34315 13 7 11.6569 7 10C7 8.34315 8.34315 7 10 7C11.6569 7 13 8.34315 13 10Z"
-                                stroke="var(--gray)"
+                                stroke="var(--light)"
                                 stroke-width="2"
                                 stroke-linecap="round"
                                 stroke-linejoin="round"
@@ -33,7 +33,7 @@ export default function AboutMe() {
                     </div>
                     <div className="detail mycourse">
                         <svg
-                            fill="var(--gray)"
+                            fill="var(--light)"
                             width="32px"
                             height="32px"
                             viewBox="0 0 16 16"
@@ -53,7 +53,7 @@ export default function AboutMe() {
                         >
                             <path
                                 d="M5 8V17.0192M9 8V17M15 8V17M19 8V17.0192M5 17.0192C5.31428 17 5.70173 17 6.2 17H17.8C18.2983 17 18.6857 17 19 17.0192M5 17.0192C4.60779 17.0431 4.32953 17.097 4.09202 17.218C3.71569 17.4097 3.40973 17.7157 3.21799 18.092C3 18.5198 3 19.0799 3 20.2V21H21V20.2C21 19.0799 21 18.5198 20.782 18.092C20.5903 17.7157 20.2843 17.4097 19.908 17.218C19.6705 17.097 19.3922 17.0431 19 17.0192M3 5.5V8H21V5.5L12 3L3 5.5Z"
-                                stroke="var(--gray)"
+                                stroke="var(--light)"
                                 stroke-width="2"
                                 stroke-linecap="round"
                                 stroke-linejoin="round"
@@ -349,11 +349,7 @@ export default function AboutMe() {
                 </div>
             </div>
             <div className="aboutme-divider"></div>
-            <ScrollDown
-                section_id="projects-section"
-                theme="dark"
-                action="PROJECTS"
-            />
+            <ScrollDown section_id="projects-section" action="PROJECTS" />
         </section>
     );
 }
