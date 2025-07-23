1158cbb18a0faf3634c7d9bb3174a3881fff88d7
seannn9 <seanulric9@gmail.com>
2025-07-23 23:33:57 +0800

    feat: added a floating sidebar for easier nav instead of scroll down btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import "./css/App.css";
 import Home from "./pages/Home/Home.jsx";
+import FloatingSidebar from "./components/FloatingSidebar/FloatingSidebar.jsx";
 
 function App() {
     return (
         <div className="main-container">
+            <FloatingSidebar />
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
