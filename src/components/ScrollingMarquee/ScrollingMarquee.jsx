e10a5ef8338a906bdc483866c550f8892236503c
seannn9 <seanulric9@gmail.com>
2025-07-25 13:43:38 +0800

    style: added skills and adjusted speed of marquee

--- src/components/ScrollingMarquee/ScrollingMarquee.jsx
+++ src/components/ScrollingMarquee/ScrollingMarquee.jsx
@@ -9,6 +9,7 @@ export default function ScrollingMarquee() {
             gradientColor="var(--dark)"
             gradientWidth={100}
             autoFill={true}
+            speed={70}
             className="scrolling-marquee"
         >
             <div className="marquee-child" style={{ display: "flex" }}>
@@ -261,8 +262,23 @@ export default function ScrollingMarquee() {
                             </clipPath>
                         </defs>
                     </svg>
-
                     <div>Node.js</div>
+                </div>
+                <div className="tech-container">
+                    <svg
+                        width="32"
+                        height="32"
+                        role="img"
+                        viewBox="0 0 24 24"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <title>GitHub</title>
+                        <path
+                            d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"
+                            fill="#fff"
+                        />
+                    </svg>
+                    <div>GitHub</div>
                 </div>
                 <div className="tech-container">
                     <svg
@@ -352,6 +368,48 @@ export default function ScrollingMarquee() {
                         </g>
                     </svg>
                     <div>Vercel</div>
+                </div>
+                <div className="tech-container">
+                    <svg
+                        width="32px"
+                        height="32px"
+                        viewBox="0 0 32 32"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <title>figma</title>
+                        <path
+                            fill-rule="evenodd"
+                            clip-rule="evenodd"
+                            d="M16 16C16 13.7909 17.7909 12 20 12C22.2091 12 24 13.7909 24 16C24 18.2091 22.2091 20 20 20C17.7909 20 16 18.2091 16 16Z"
+                            fill="#1ABCFE"
+                        />
+                        <path
+                            fill-rule="evenodd"
+                            clip-rule="evenodd"
+                            d="M8 24C8 21.7909 9.79086 20 12 20H16V24C16 26.2091 14.2091 28 12 28C9.79086 28 8 26.2091 8 24Z"
+                            fill="#0ACF83"
+                        />
+                        <path
+                            fill-rule="evenodd"
+                            clip-rule="evenodd"
+                            d="M16 4V12H20C22.2091 12 24 10.2091 24 8C24 5.79086 22.2091 4 20 4H16Z"
+                            fill="#FF7262"
+                        />
+                        <path
+                            fill-rule="evenodd"
+                            clip-rule="evenodd"
+                            d="M8 8C8 10.2091 9.79086 12 12 12H16V4H12C9.79086 4 8 5.79086 8 8Z"
+                            fill="#F24E1E"
+                        />
+                        <path
+                            fill-rule="evenodd"
+                            clip-rule="evenodd"
+                            d="M8 16C8 18.2091 9.79086 20 12 20H16V12H12C9.79086 12 8 13.7909 8 16Z"
+                            fill="#A259FF"
+                        />
+                    </svg>
+                    <div>Figma</div>
                 </div>
                 <div className="tech-container">
                     <svg
