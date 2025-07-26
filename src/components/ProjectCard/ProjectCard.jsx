4d999221f7521b20a1fce69761d9911a9a0a6137
seannn9 <seanulric9@gmail.com>
2025-07-26 19:19:25 +0800

    style: added project image card fetched from supabase bucket

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -1,9 +1,11 @@
 import "./ProjectCard.css";
 
-export default function ProjectCard({ project_name }) {
+export default function ProjectCard({ project_image, project_name }) {
     return (
         <div className="card">
-            <div className="project-image"></div>
+            <div className="project-image">
+                <img src={project_image} alt={project_name} />
+            </div>
             <div className="project-details">
                 <h3>{project_name}</h3>
                 <div className="row-2">
